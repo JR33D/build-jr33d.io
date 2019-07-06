4276419f617179477a7773154a23852a37e37226
Jeremy Reed <JReed129@gmail.com>
2019-07-06 13:19:19 -0400

    clean up portfolio page and comment out blog for now.
    * Add external links
    * Update images
    * Update styling and formatting of pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,11 +9,11 @@ import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
     { path: 'home', component: HomeComponent },
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'resume', component: ResumeComponent },
     { path: 'portfolio', component: PortfolioComponent },
-    { path: 'blog', component: BlogComponent },
-    { path: 'contact', component: ContactComponent }
+    // { path: 'blog', component: BlogComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: '**', redirectTo: 'home', pathMatch: 'full' },
 ];
 
 @NgModule({
